5e8fed75c0fd5d5338968de3c1814a72e3859e26
Edoardo Lovino <81745980+edoloba@users.noreply.github.com>
2022-03-04 16:39:26 +0100

    add files

---
+++ 12-Node in Single Page Application/client/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
+
+const App = () => {
+    return(
+        <>
+        <h1>Hello peeps</h1>
+        <button>send</button>
+        </>
+    )
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
